a394cc1eb7fe5dfca871150b664533d9950b230e
jeventuro <jeventurora@ucvvirtual.edu.pe>
2025-12-31 20:02:48 -0500

    se actualiza en firebaseconfig

--- services/firebaseConfig.ts
+++ services/firebaseConfig.ts
@@ -3,14 +3,16 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
+
+const env = (import.meta as any).env || {};
 // Reemplaza estos valores con los de tu consola de Firebase
 const firebaseConfig = {
-  apiKey: process.env.VITE_API_KEY,
-  authDomain: process.env.VITE_AUTH_DOMAIN,
-  projectId: process.env.VITE_PROJECT_ID,
-  storageBucket: process.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VITE_MESSAGING_SENDER_ID,
-  appId: process.env.VITE_APP_ID
+  apiKey: env.VITE_API_KEY,
+  authDomain: env.VITE_AUTH_DOMAIN,
+  projectId: env.VITE_PROJECT_ID,
+  storageBucket: env.VITE_STORAGE_BUCKET,
+  messagingSenderId: env.VITE_MESSAGING_SENDER_ID,
+  appId: env.VITE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
